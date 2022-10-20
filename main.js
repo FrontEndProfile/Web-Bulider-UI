f11125f5320583dadb1463e9daa05d76955ae42e
VigoFazal <frontendprofile@gmail.com>
2022-10-20 07:42:07 -0700

    v-01

---
+++ main.js
@@ -0,0 +1,10 @@
+function openForm() {
+    document.getElementById("edit-tools").style.display = "block";
+    document.querySelector(".open_modal").style.display = "none";
+  }
+  
+  function closeForm() {
+    document.getElementById("edit-tools").style.display = "none";
+    document.querySelector(".open_modal").style.display = "block";
+
+  }
